ca1352111e2cf65fbd5de668a29c04f899d7a0a4
stefanie ramaekers <ramaekersstefanie@hotmail.com>
2016-11-09 23:47:01 +0100

    update & create data storage excercises

--- UserInfo File/app/src/main/java/com/example/stefanie/userinfo/MainActivity.java
+++ UserInfo File/app/src/main/java/com/example/stefanie/userinfo/MainActivity.java
@@ -1,13 +1,92 @@
 package com.example.stefanie.userinfo;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String filename = "userinformation";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText nameTextbox = ((EditText) findViewById(R.id.editTextName));
+        EditText addressTextbox = ((EditText) findViewById(R.id.editTextAddress));
+        EditText phoneTextbox = ((EditText) findViewById(R.id.editTextPhone));
+        EditText emailTextbox = ((EditText) findViewById(R.id.editTextEmail));
+        CheckBox newsletterCheckBox = ((CheckBox) findViewById(R.id.cbNewsletter));
+        try{
+            FileInputStream inputStream = openFileInput(filename);
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine())!= null){
+                stringBuilder.append(line + "\n");
+            }
+            String[] textboxValues = stringBuilder.toString().split("[\n]+");
+            nameTextbox.setText(textboxValues[0]);
+            addressTextbox.setText(textboxValues[1]);
+            phoneTextbox.setText(textboxValues[2]);
+            emailTextbox.setText(textboxValues[3]);
+            if (textboxValues[4].equals("getsNewsletter")){
+                newsletterCheckBox.setChecked(true);
+            }
+            else {
+                newsletterCheckBox.setChecked(false);
+            }
+            inputStream.close();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        Button okbtn = (Button) findViewById(R.id.SaveBtn);
+        okbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveToFile();
+            }
+        });
+    }
+
+    private void saveToFile() {
+        FileOutputStream outputStream;
+        String separator = System.getProperty("line.separator");
+
+        String name, address, phone, email;
+        name = ((EditText) findViewById(R.id.editTextName)).getText().toString();
+        address = ((EditText) findViewById(R.id.editTextAddress)).getText().toString();
+        phone = ((EditText) findViewById(R.id.editTextPhone)).getText().toString();
+        email = ((EditText) findViewById(R.id.editTextEmail)).getText().toString();
+        boolean getsNewsletter = ((CheckBox) findViewById(R.id.cbNewsletter)).isChecked();
+
+        try {
+            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            String fileContent = name + separator + address + separator + phone + separator + email + separator;
+            if (getsNewsletter){
+                fileContent += "getsNewsletter";
+            } else{
+                fileContent += "doesntGetNewsletter";
+            }
+            outputStream.write(fileContent.getBytes());
+            outputStream.close();
+            Toast.makeText(getApplicationContext(), "Saved settings!", Toast.LENGTH_SHORT).show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- VolumeExternal/app/src/main/java/com/example/stefanie/volume/MainActivity.java
+++ VolumeExternal/app/src/main/java/com/example/stefanie/volume/MainActivity.java
@@ -1,13 +1,123 @@
 package com.example.stefanie.volume;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String filename = "preferences";
+    SeekBar beltoon, media, meldingen, alarm;
+    CheckBox cbCombi;
+    String value;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        beltoon = (SeekBar) findViewById(R.id.seekBarBeltoon);
+        media = (SeekBar) findViewById(R.id.seekBarMedia);
+        meldingen = (SeekBar) findViewById(R.id.seekBarMelding);
+        alarm = (SeekBar) findViewById(R.id.seekBarAlarm);
+        cbCombi = (CheckBox) findViewById(R.id.checkBox);
+        Button okButton = (Button) findViewById(R.id.button);
+        readFromExternal();
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveToExternal();
+            }
+        });
+    }private void readFromExternal() {
+        if (isExternalStorageReadable()) {
+            try {
+                File file = new File(Environment.getExternalStorageDirectory(), filename);
+                FileInputStream inputStream = new FileInputStream(file);
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line + "\n");
+                }
+                String[] textboxValues = stringBuilder.toString().split("[\n]+");
+                value = textboxValues[0];
+                beltoon.setProgress(Integer.parseInt(value));
+                value = textboxValues[1];
+                alarm.setProgress(Integer.parseInt(value));
+                value = textboxValues[2];
+                media.setProgress(Integer.parseInt(value));
+                value = textboxValues[3];
+                meldingen.setProgress(Integer.parseInt(value));
+                if (textboxValues[4].equals("combiJa")){
+                    cbCombi.setChecked(true);
+                }else{
+                    cbCombi.setChecked(false);
+                }
+                inputStream.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void saveToExternal() {
+        if (isExternalStorageWritable()) {
+            File file = new File(Environment.getExternalStorageDirectory(), filename);
+            FileOutputStream outputStream;
+            String separator = System.getProperty("line.separator");
+            int beltoonVolume = beltoon.getProgress();
+            int alarmVolume = alarm.getProgress();
+            int mediaVolume = media.getProgress();
+            int meldingenVolume = meldingen.getProgress();
+            boolean combiBeltoonMeldingen = cbCombi.isChecked();
+
+            try {
+                outputStream = new FileOutputStream(file);
+                String fileContent = beltoonVolume + separator + alarmVolume + separator + mediaVolume + separator + meldingenVolume + separator;
+                if (combiBeltoonMeldingen) {
+                    fileContent += "combiJa";
+                } else {
+                    fileContent += "combiNee";
+                }
+                outputStream.write(fileContent.getBytes());
+                outputStream.close();
+                Toast.makeText(getApplicationContext(), "Saved settings!", Toast.LENGTH_SHORT).show();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    /* Checks if external storage is available to at least read */
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
     }
 }
